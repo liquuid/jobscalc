935a10c0de38ce0c0475071375f74c4aed8702ad
Fernando Henrique <liquuid@gmail.com>
2021-07-18 15:53:54 -0300

    placeholder content

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,18 @@ const routes = express.Router();
 
 const views = __dirname + '/views/'
 
-routes.get('/', (req, res) => res.render(views + "index.ejs"))
-routes.get('/job', (req, res) => res.render(views + "job.ejs"))
-routes.get('/job/edit', (req, res) => res.render(views + "job-edit.ejs"))
-routes.get('/profile', (req, res) => res.render(views + "profile.ejs"))
+
+const profile = {
+    name: "Jakelyn",
+    avatar: "https://avatars.githubusercontent.com/u/17316392?s=460&u=6912a91a70bc89745a2079fdcdad3bc3ce370f13&v=4",
+    "monthly-budget": 3000,
+    "days-per-week": 5,
+    "hours-per-day": 5,
+    "vacation-per-year": 4
+}
+routes.get('/', (req, res) => res.render(views + "index"))
+routes.get('/job', (req, res) => res.render(views + "job"))
+routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
+routes.get('/profile', (req, res) => res.render(views + "profile", { profile }))
 
 module.exports = routes;
