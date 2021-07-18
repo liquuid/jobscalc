bead8ee1b4b3adc3a250a77d5f5030fcd6509e20
Fernando Henrique <liquuid@gmail.com>
2021-07-18 15:23:54 -0300

    fix file paths

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routes = express.Router(); 
+
+const basePath = __dirname + '/views'
+
+routes.get('/', (request, response) => response.sendFile(basePath + "/index.html"))
+routes.get('/job', (request, response) => response.sendFile(basePath + "/job.html"))
+routes.get('/job/edit', (request, response) => response.sendFile(basePath + "/job-edit.html"))
+routes.get('/profile', (request, response) => response.sendFile(basePath + "/profile.html"))
+
+routes.get('/index.html', (request, response) => {
+    return response.redirect('/')
+})
+module.exports = routes;
